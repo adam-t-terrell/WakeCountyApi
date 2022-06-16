9f2b61a8101f69efe88008e85290b1ef7c9ec5ce
Adam Terrell <33033305+adam-t-terrell@users.noreply.github.com>
2022-06-16 09:46:56 -0500

    only return Last Name, First Name and Department, sorted by Last Name then First Name

--- Program.cs
+++ Program.cs
@@ -13,16 +13,24 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var employees = new List<Employee>() {
-    new Employee(1, "Jackson", "Alberta", "Finance", DateTime.Parse("6/5/2007")),
-    new Employee(2, "Bennett", "Alicia", "Human Resources", DateTime.Parse("4/15/2001")),
-    new Employee(3, "Avent", "Donna", "Revenue", DateTime.Parse("4/20/2009")),
-    new Employee(4, "Holder", "Duane", "Human Services", DateTime.Parse("8/15/2020"))
+var employees = new List<EmployeeResource>() {
+    new EmployeeResource(1, "Jackson", "Alberta", "Finance", DateTime.Parse("6/5/2007")),
+    new EmployeeResource(2, "Bennett", "Alicia", "Human Resources", DateTime.Parse("4/15/2001")),
+    new EmployeeResource(3, "Avent", "Donna", "Revenue", DateTime.Parse("4/20/2009")),
+    new EmployeeResource(4, "Holder", "Duane", "Human Services", DateTime.Parse("8/15/2020"))
 };
 
 app.MapGet("/employees", () =>
 {
-    return employees;
+    return employees
+        .OrderBy(employee => employee.LastName)
+        .ThenBy(employee => employee.FirstName)
+        .Select(employee => new Employee()
+        {
+            LastName = employee.LastName,
+            FirstName = employee.FirstName,
+            Department = employee.Department
+        });
 })
 .WithName("GetEmployees");
 
@@ -35,10 +43,16 @@ app.MapGet("/employees/{id}", (int id) =>
 app.MapPost("/employees", (string LastName, string FirstName, string Department, DateTime HireDate) =>
 {
     var id = employees.OrderByDescending(employee => employee.Id).First().Id + 1;
-    employees.Add(new Employee(id, LastName, FirstName, Department, HireDate));
+    employees.Add(new EmployeeResource(id, LastName, FirstName, Department, HireDate));
 })
 .WithName("AddEmployee");
 
 app.Run();
 
-record Employee(int Id, string LastName, string FirstName, string Department, DateTime HireDate);
+record EmployeeResource(int Id, string LastName, string FirstName, string Department, DateTime HireDate);
+
+public class Employee {
+    public string? LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? Department { get; set; }
+}
